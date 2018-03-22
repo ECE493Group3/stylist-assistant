b73cb52dbe7526e316e0914b04f14f3d2194c2c5
rwu780 <rwu4@ualberta.ca>
2018-03-22 15:19:21 -0600

    Attach category_label to each image. Create Hirearchy tree. Need to implement similarity function based on tree

--- recommender.py
+++ recommender.py
@@ -7,9 +7,17 @@
 # This generates a test sample of the data for the Top/Bottom/Full-body task,
 # as a tsv file
 
+import collections
 import os
 import sys
 import random
+
+import anytree
+from anytree import Node, RenderTree
+
+
+# import pymongo
+# from pymongo import MongoClient
 
 DATA_DIRECTORY = 'image-processing/DATA'
 
@@ -19,19 +27,22 @@ LIST_CATEGORY_CLOTH_FILE = os.path.join(DATA_DIRECTORY, 'Anno', 'list_category_c
 LIST_ATTR_IMG_FILE = os.path.join(DATA_DIRECTORY, 'Anno', 'list_attr_img.txt')
 LIST_ATTR_CLOTH_FILE = os.path.join(DATA_DIRECTORY, 'Anno', 'list_attr_cloth.txt')
 
-TSV_FILE_TRAIN = 'sample_category_img_train.txt'
-TSV_FILE_VALIDATION = 'sample_category_img_validation.txt'
+TSV_FILE_VALIDATION = 'sample_cloth_sim.txt'
 
 class Cloth:
 
-	def __init__(self, img_file, cat_type):
+	def __init__(self, img_file, cat_type, cat_label, attr_v = [], attr_label={}):
 		self.img_file = img_file
 		self.cat_type = cat_type
-		self.attr_v = []
-		self.attr_label = {}
+		self.cat_label = cat_label
+		self.attr_v = attr_v
+		self.attr_label = attr_label
 
 	def get_cat_type(self):
 		return self.cat_type
+
+	def get_cat_label(self):
+		return self.cat_label
 
 	def get_attr_label(self):
 		return self.attr_label
@@ -48,16 +59,50 @@ class Cloth:
 
 		for i, attr in enumerate(self.attr_v):
 			if(attr == "1"):                
-				temp = {i+1:attr_type[int(i+1)]}
+				temp = {str(i+1):attr_type[int(i+1)]}
 				self.attr_label.update(temp)
 
 	def __str__(self):
 		img_file_s = "img_file: " + str(self.img_file) + "\n"
 		cat_type_s = "cat_type: " + str(self.cat_type) + "\n"
+		cat_label_s = "cat_label: " + str(self.cat_label) + "\n"
 		attr_label_s = "attr_label: " + str(self.attr_label) + "\n"
-		attr_v_s = "attr_v: " + str(self.attr_v) + "\n"
+		# attr_v_s = "attr_v: " + str(self.attr_v) + "\n"
 
-		return img_file_s + cat_type_s + attr_label_s + attr_v_s
+		return img_file_s + cat_type_s + cat_label_s + attr_label_s
+
+def create_nodes():
+
+	# Root Node
+	root_cloth = Node("Cloth")
+	cloth_dict = {
+		"Cloth": ["Tops", "Bottoms", "Fullbody"],
+		"Tops": ["Sweater", "Jacket", "Tee"],
+		"Bottoms": ["Pants", "Sweatpants", "Shorts", "Skirt"],
+		"Fullbody": ["Coat", "Onesie", "Dress"],
+		"Sweater":["Blazer", "Bomber", "Cardigan", "Hoodie", "Turtleneck", "Flannel"],
+		"Jacket": ["Parka", "Peacoat", "Poncho", "Anorak"],
+		"Tee": ["Top", "Blouse", "Jersey", "Button-Down", "Tank", "Henley", "Halter"],
+		"Pants": ["Jeans", "Chinos", "Joggers"],
+		"Sweatpants": ["Leggings", "Culottes", "Jodhpurs", "Capris", "Jeggins"],
+		"Shorts": ["Cutoffs", "Trunks", "Sweatshorts"],
+		"Skirt": ["Cauchos", "Sarong"],
+		"Coat": ["Robe", "Cape"],
+		"Onesie": ["Jumpsuit", "Romper"],
+		"Dress": ["Coverup", "Caftan", "Kimono", "Kaftan", "Nightdress", "Shirtdress", "Sundress"],
+	}
+
+	insert_order = ["Cloth", "Tops", "Bottoms", "Fullbody", "Sweater", "Jacket", "Tee", "Pants", "Sweatpants", "Shorts", "Skirt", "Coat", "Onesie", "Dress"]
+
+	for parent in insert_order:
+		parent_node = anytree.find(root_cloth, lambda node: node.name == parent)
+
+		for node in cloth_dict[parent]:
+			x =Node(node, parent = parent_node)
+
+	# for pre, fill, node in RenderTree(root_cloth):
+	# 	print("%s%s" % (pre, node.name))
+	return root_cloth
 
 def get_category_types():
 
@@ -65,8 +110,8 @@ def get_category_types():
 
 	with open(LIST_CATEGORY_CLOTH_FILE) as f:
 		for i, line in enumerate(f.readlines()[2:]):
-			_, typ = line.split()
-			cat_type[i + 1] = int(typ)
+			label, typ = line.split()
+			cat_type[i + 1] = [int(typ), label]
 
 	return cat_type
 
@@ -83,31 +128,6 @@ def get_attr_types():
 
 	return attr_type
 
-# def get_category(category_type):
-#   with open(LIST_CATEGORY_IMG_FILE) as f:
-#       lines = f.readlines()
-#       for i, line in enumerate(lines[2:]):
-#           imgfile, cat = line.split()
-#           temp = Cloth(imgfile, category_type[int(cat)])
-#           # temp.set_img_file(imgfile)
-#           # temp.set_cat_type(category_type[int(cat)])
-#           cloths_list.append(temp)
-
-# def get_attrbute_label(attr_type):
-#   with open(LIST_ATTR_IMG_FILE) as f:
-#       lines = f.readlines()
-
-#       for i, line in enumerate(lines[2:]):
-#           break_index = line.find(' ')
-#           imgfile = line[:break_index+1]
-
-#           attr_v_s = line[break_index:]
-#           attr_v_s = attr_v_s.replace('0', '0.5')
-#           attr_v_s = attr_v_s.replace('-1', '0')
-
-#           attr_v = attr_v_s.strip().split()
-#           cloths_list[i].set_attr_v(attr_v, attr_type)
-
 def get_category_attribute_label(category_type, attr_type):
 	with open(LIST_CATEGORY_IMG_FILE) as cat_f, open(LIST_ATTR_IMG_FILE) as attr_f:
 		cat_lines = cat_f.readlines()
@@ -123,7 +143,9 @@ def get_category_attribute_label(category_type, attr_type):
 				attr_v_s = attr_v_s.replace('-1', '0')
 				attr_v = attr_v_s.split()
 
-				temp = Cloth(cat_imgfile, category_type[int(cat)])
+
+
+				temp = Cloth(cat_imgfile, category_type[int(cat)][0], category_type[int(cat)][1])
 				temp.set_attr_v(attr_v, attr_type)
 				cloths_list.append(temp)
 
@@ -173,19 +195,65 @@ if __name__=="__main__":
 	print("Making tsv")
 	sample_size = int(sys.argv[1])
 
+	print("Create Nodes")
+	root_cloth = create_nodes()
+
+	# db_host = '127.0.0.1'
+	# db_port = 27017
+	# print("Connecting to DB" + db_host + " " + str(db_port))
+
+	# # Connect to Mongodb
+	# client = MongoClient(db_host, db_port)
+	
+	# # Getting DB
+	# db = client['stylist_db']
+	
+	# # Getting Collection
+	# collection = db['stylist']
+	# cloths_length = collection.count()
+
+	# print("Number of items in db: " + str(cloths_length))
+
 	cat_types = get_category_types()
 	attr_types = get_attr_types()
 	get_category_attribute_label(cat_types, attr_types)
-	
-	a = random.randint(0, len(cloths_list))
-	cloth_a = cloths_list[a]
+
+	# if(cloths_length == 0):
+	# 	cat_types = get_category_types()
+	# 	attr_types = get_attr_types()
+	# 	get_category_attribute_label(cat_types, attr_types)
+
+	# 	for i, cloth in enumerate(cloths_list):
+	# 		input_cloth = {
+	# 			"_id": str(i),
+	# 			"img_file": cloth.get_img_file(),
+	# 			"cat_type": cloth.get_cat_type(),
+	# 			# "attr_v": cloth.get_attr_v(),
+	# 			"attr_label": cloth.get_attr_label(),
+	# 		}
+	# 		collection.insert_one(input_cloth)
+
+	# 	print("Finish uploading to DB")
+	with open(TSV_FILE_VALIDATION, 'w') as f:
+		f.writelines(str(cloth) for cloth in cloths_list)
+
+	exit(1)
 
 	with open(TSV_FILE_VALIDATION, 'w') as f:
-		for i in range(0, len(cloths_list)):
+		for j in range(0, sample_size):
+			a = random.randint(0, len(cloths_list))
+			# doc = collection.find_one({"_id": str(a)})
+			# cloth_a = Cloth(doc['img_file'], doc['cat_type'], doc['attr_v'], doc['attr_label'])
+		
+			cloth_a = cloths_list[a]
 			
-			cloth_b = cloths_list[i]
-			sim = get_item_similarity(cloth_a, cloth_b)
-			f.write("Comparing: " + cloth_a.get_img_file() + ", " + cloth_b.get_img_file() + "\n Similarity: " + str(sim) + '\n')
+			for i in range(0, len(cloths_list)):
+				# doc_b = collection.find_one({"_id":str(i)})
+				# cloth_b = Cloth(doc_b['img_file'], doc_b['cat_type'], doc_b['attr_v'], doc_b['attr_label'])
+				cloth_b = cloths_list[i]
+				sim = get_item_similarity(cloth_a, cloth_b)
+
+				f.write("Comparing: " + str(cloth_a) + " " + str(cloth_b) + "\n Similarity: " + str(sim) + '\n')
 
 	print("Done")
 
