ba520f9817fb6cde2fa0c6387c4a1d8f44d547a5
Vitor <mendona@ualberta.ca>
2018-03-28 23:24:23 +0000

    Adapt training script to selected categories

--- category_tagging_nn.py
+++ category_tagging_nn.py
@@ -8,22 +8,37 @@ import cnn_vgg16
 tf.logging.set_verbosity(tf.logging.INFO)
 
 DATA_DIRECTORY = os.path.join('DATA', 'Img_compressed')
-SAMPLE_CATEGORY_IMG_FILE_TRAIN = "sample_category_img_train.txt"
-SAMPLE_CATEGORY_IMG_FILE_VALIDATION = "sample_category_img_validation.txt"
+SAMPLE_CATEGORY_IMG_FILE_TRAIN = "sample_equal_numbers_train.txt"
+SAMPLE_CATEGORY_IMG_FILE_VALIDATION = "sample_equal_numbers_validation.txt"
 
-def parse_images(filename):
+LOG_FILE = "category_training.log"
+
+BATCH_SIZE = 20
+
+def parse_images(filename, n_epochs=1):
     images = []
     labels = []
     with open(filename, 'r') as tsv_f:
         for line in tsv_f.readlines():
-            imgfile, cat = line.split('\t')
-            labels.append(int(cat) - 1)
+            imgfile, cat_index = line.split('\t')
+            labels.append(int(cat_index))
 
             full_path = os.path.join(DATA_DIRECTORY, imgfile)
             images.append(full_path)
 
     dataset = tf.data.Dataset.from_tensor_slices((images, labels))
-    return dataset.shuffle(len(images) + 1).map(cnn_vgg16.pre_process_training_data).batch(20)
+    if n_epochs > 1:
+        dataset = dataset.repeat(n_epochs - 1)
+
+    dataset_shuffled = dataset.shuffle(len(images) * n_epochs)
+    dataset_pre_processed = dataset_shuffled.map(cnn_vgg16.pre_process_training_data)
+    return dataset_pre_processed.batch(BATCH_SIZE)
+
+
+def log_results(n_epochs, eval_results):
+    with open(LOG_FILE, 'a') as f:
+        f.write("After {} more epochs:\n".format(n_epochs))
+        f.write("{}\n".format(eval_results))
 
 
 def main(argv):
@@ -32,12 +47,16 @@ def main(argv):
 
     train_file = SAMPLE_CATEGORY_IMG_FILE_TRAIN
     validation_file = SAMPLE_CATEGORY_IMG_FILE_VALIDATION
+    n_epochs = 1
 
     if '-t' in argv:
         train_file = argv[argv.index('-t') + 1]
 
     if '-v' in argv:
         validation_file = argv[argv.index('-v') + 1]
+
+    if '-e' in argv:
+        n_epochs = int(argv[argv.index('-e') + 1])
 
     print()
     if should_train:
@@ -61,13 +80,15 @@ def main(argv):
 
     if should_train:
         top_bottom_classifier.train(
-                input_fn=lambda: parse_images(train_file),
+                input_fn=lambda: parse_images(train_file, n_epochs),
                 hooks=[logging_hook])
 
     eval_results = top_bottom_classifier.evaluate(
             input_fn=lambda: parse_images(validation_file))
 
     print(eval_results)
+    if should_train:
+        log_results(n_epochs, eval_results)
 
 if __name__ == "__main__":
     tf.app.run(argv=sys.argv)
